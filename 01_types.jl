4439e9f59bb96ea475db3ac09ed775bef3b753e4
gaelforget <gforget@mit.edu>
2019-09-22 23:30:08 -0400

    improve comments and add a few things

--- 01_types.jl
+++ 01_types.jl
@@ -13,25 +13,53 @@
 #     name: julia-1.1
 # ---
 
-# ## `MeshArrays.jl` represents global grids where multiple arrays are connected at their edges as a new data type
+# ## `MeshArrays` represent global gridded variables
 #
-# Load the `MeshArrays.jl` package
+# Each `MeshArray` contains an array of elementary arrays that (1) are connected at their edges and (2) collectively form a global grid. Global grid specifications are contained within `gcmgrid` instances.
+#
+# #### First, let's load the `MeshArrays.jl` package
 
 using MeshArrays
 
-# Select a pre-defined grid; `LLC90` is used here
+# #### Select a pre-defined grid such as `LLC90`
 
 mygrid=GridSpec("LLC90")
 
-# Download the pre-defined grid if needed
+# And download the pre-defined grid if needed
 
-if !isdir("GRID_LLC90") 
-    run(`git clone https://github.com/gaelforget/GRID_LLC90`)
-end
+!isdir("GRID_LLC90") ? run(`git clone https://github.com/gaelforget/GRID_LLC90`) : nothing
 
+# #### Read a MeshArray from file
+#
 # Read a global field from binary file and convert it to `MeshArrays`'s `gcmfaces` type
 
 D=mygrid.read(mygrid.path*"Depth.data",MeshArray(mygrid,Float64))
 show(D)
+
+# The read / write functions can also be used to convert a MeshArray from / to Array
+
+tmp1=write(D)
+tmp2=read(tmp1,D)
+
+# #### MeshArrays should behave just like Arrays
+#
+# Here are a few examples that would be coded similarly in both cases
+
+# +
+size(D)
+eltype(D)
+view(D,:)
+
+D .* 1.0
+D .* D
+1000*D
+D*1000
+
+D[findall(D .> 300.)] .= NaN
+D[findall(D .< 1.)] .= NaN
+
+D[1]=0.0 .+ D[1]
+tmp=cos.(D)
+# -
 
 
