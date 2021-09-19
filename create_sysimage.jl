b4461bfa79cbd04cba707c11778d7c08abcca8c9
gaelforget <gforget@mit.edu>
2021-09-19 09:57:41 -0400

    rm pkgs that are not in Project.toml

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,5 +1,5 @@
 using PackageCompiler
-create_sysimage([:Pluto, :PlutoUI, :Plots, :UnicodePlots, :ClimateModels, :StochasticDiffEq];
+create_sysimage([:Pluto, :PlutoUI, :Plots, :ClimateModels];
                 precompile_execution_file = "warmup.jl",
                 replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
