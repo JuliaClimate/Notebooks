ea99312c2c977ae3f121fbaa2cf59f7fd129efcc
gaelforget <gforget@mit.edu>
2021-09-20 18:29:47 -0400

    try with all project packages

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,5 +1,8 @@
 using PackageCompiler
-create_sysimage([:Pluto, :MeshArrays, :NCTiles, :NetCDF, :OceanStateEstimation];
+create_sysimage([:CFTime, :CSV, :ClimateModels, :ColorSchemes, :DataFrames, :FortranFiles, 
+                 :IJulia, :IndividualDisplacements, :MITgcmTools, :MeshArrays, :NCTiles, 
+                 :NetCDF, :OceanStateEstimation, :Plots, :Pluto, :PlutoUI, :Printf, 
+                 :UnicodePlots, :WGLMakie];
                 precompile_execution_file = "warmup.jl",
                 replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
