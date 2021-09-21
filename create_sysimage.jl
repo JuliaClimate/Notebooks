b9c9a6b7364852f0d1dd4ad54e2d4c33d59adbef
gaelforget <gforget@mit.edu>
2021-09-20 20:48:43 -0400

    another try

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,8 +1,7 @@
 using PackageCompiler
-create_sysimage([:CFTime, :CSV, :ClimateModels, :ColorSchemes, :DataFrames, :FortranFiles, 
-                 :IJulia, :IndividualDisplacements, :MITgcmTools, :MeshArrays, :NCTiles, 
-                 :NetCDF, :OceanStateEstimation, :Plots, :Pluto, :PlutoUI, :Printf, 
-                 :UnicodePlots, :WGLMakie];
+create_sysimage([:CSV, :ClimateModels, :DataFrames, 
+                 :IJulia, :IndividualDisplacements, :MITgcmTools, 
+                 :Plots, :Pluto, :PlutoUI, :UnicodePlots, :WGLMakie];
                 precompile_execution_file = "warmup.jl",
                 replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
