97bc711e41b5ba32586f6e6e32def2f4545a2151
gaelforget <gforget@mit.edu>
2021-09-22 01:25:42 -0400

    try prebuilding hector

---
+++ sysimage/pre_build_models.jl
@@ -0,0 +1,13 @@
+
+using PlutoSliderServer, ClimateModels
+pth=dirname(pathof(ClimateModels))
+fil=joinpath(pth,"..","examples","Hector.jl")
+PlutoSliderServer.export_notebook(fil)
+
+tmp1=readdir(tempdir())
+tst1=[isfile(joinpath(tempdir(),i,"hector","src","hector")) for i in tmp1]
+tst2=[!islink(joinpath(tempdir(),i,"hector","src","hector")) for i in tmp1]
+ii=findall(tst1.*tst2)[1]
+symlink(joinpath(tempdir(),tmp1[ii],"hector","src","hector"),"hector")
+
+
