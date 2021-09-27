6b24fd6a26acb7ff9eaeb40f70c561fcea19b7c5
gaelforget <gforget@mit.edu>
2021-09-27 15:44:41 -0400

    improve notebook_03.jl

--- IPCC/notebook_03.jl
+++ IPCC/notebook_03.jl
@@ -154,7 +154,7 @@ begin
 	set_col!(df,"SSA",colors,:lightgray)
 	set_col!(df,"CAF",colors,:lightgray)
 
-	ttl="a) Synthesis of assessment of observed change in hot extremes \n and confidence in human contribution to the observed changes in the world’s regions"
+	ttl="a) Synthesis of assessment of observed change in hot extremes \n and confidence in human contribution to the observed changes \n in the world’s regions"
 
 	confidencelevels=fill("⚪?",size(df,1)) 	
 	cl=["⚫⚫⚫","⚫⚫","⚫","⚪"]
@@ -175,7 +175,48 @@ end
 # ╔═╡ 7e70284e-d982-4f8f-be1c-688499267928
 let
 	g = Figure()
-	a = Axis(g[1, 1],title=ttl)
+
+	a = Axis(g[1, 1])
+	set_theme!(theme_light())
+
+	h=25
+
+	txt="Type of observed change \nin hot extremes"	
+	t = text!(a, txt,
+			position = Point2f(0,h*0.9),
+			align = (:left, :baseline),
+			justification = :left,
+			color = :black,
+		    textsize = 12)	
+	
+	poly!(hexa(1,h*0.8,0.5), color = :lightcoral, strokecolor = :black, strokewidth = 1)
+	text!(a,"Increase (41)",position = Point2f(2,h*0.78),textsize = 10)
+	poly!(hexa(1,h*0.7,0.5), color = :lightblue, strokecolor = :black, strokewidth = 1)
+	text!(a,"Decrease (0)",position = Point2f(2,h*0.68),textsize = 10)
+	poly!(hexa(1,h*0.6,0.5), color = Pattern("/"), strokecolor = :black, strokewidth = 1)
+	text!(a,"Low agreement in the \n type of change (2)",position = Point2f(2,h*0.58),textsize = 10)
+	poly!(hexa(1,h*0.5,0.5), color = :lightgray, strokecolor = :black, strokewidth = 1)
+	text!(a,"Limited data and/or \n literature (2)",position = Point2f(2,h*0.48),textsize = 10)
+
+	txt="Confidence in human \ncontribution to the \nobserved change"
+	t = text!(a, txt,
+			position = Point2f(0,h*0.3),
+			align = (:left, :baseline),
+			justification = :left,
+			color = :black,
+		    textsize = 12)	
+
+	text!(a,cl[1]*" High",position = Point2f(1,h*0.24),textsize = 10)
+	text!(a,cl[2]*"  Medium",position = Point2f(1,h*0.20),textsize = 10)
+	text!(a,cl[3]*"    Low due to limited agreement",position = Point2f(1,h*0.16),textsize = 10)
+	text!(a,cl[4]*"    Low due to limited evidence",position = Point2f(1,h*0.12),textsize = 10)
+
+	hidespines!(a)
+	hidedecorations!(a)
+	xlims!(0,10)
+	ylims!(0,h)
+
+	a = Axis(g[1, 2],title=ttl)
 	set_theme!(theme_minimal())
 	for i in 1:size(df,1)
 		if iseven(df[i,:y])
@@ -213,10 +254,17 @@ let
 
 	end
 
+	text!(a,"Type of observed change since the 1950s",position = Point2f(0,-5),
+		textsize = 16,align = (:center, :baseline), justification = :center)
+	
 	hidespines!(a)
 	hidedecorations!(a)
 	xlims!(-7,8)
-	ylims!(-4.5,4.5)
+	ylims!(-6.0,4.0)
+	
+	colsize!(g.layout, 1, Relative(1/5))
+	
+	#save("fig3.png", g)
 	
 	g
 end
