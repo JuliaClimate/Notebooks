2157e25998653157b072938597a7d68880e01671
gaelforget <gforget@mit.edu>
2021-10-17 08:33:19 -0400

    try commenting out speedy prebuild

--- sysimage/pre_build_models.jl
+++ sysimage/pre_build_models.jl
@@ -18,6 +18,7 @@ end
 
 ## Speedy
 
+if false
 fil=joinpath(pth,"..","examples","Speedy.jl")
 PlutoSliderServer.export_notebook(fil)
 
@@ -25,6 +26,7 @@ tmp1=readdir(tempdir())
 tst1=[isfile(joinpath(tempdir(),i,"bin","speedy")) for i in tmp1]
 ii=findall(tst1)[1]
 symlink(joinpath(tempdir(),tmp1[ii],"bin","speedy"),"speedy")
+end
 
 ## MITgcm
 
