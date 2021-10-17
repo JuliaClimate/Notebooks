c007944e42a89a5605be646bbd5a994581d30444
gaelforget <gforget@mit.edu>
2021-10-17 01:04:23 -0400

    try with sysimage_path

--- sysimage/create_sysimage.jl
+++ sysimage/create_sysimage.jl
@@ -3,5 +3,5 @@ create_sysimage([:CSV, :ClimateModels, :DataFrames,
                  :IJulia, :IndividualDisplacements, :MITgcmTools, 
                  :Plots, :Pluto, :PlutoUI, :UnicodePlots, :CairoMakie];
                 precompile_execution_file = "sysimage/warmup.jl",
-                replace_default = true,
+                sysimage_path="ExampleSysimage.so",
                 cpu_target = PackageCompiler.default_app_cpu_target())
