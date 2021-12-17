f6b87435f792758423129d289bc61d6e132e5710
gaelforget <gforget@mit.edu>
2021-12-17 09:58:09 -0500

    try longer timeout for download

--- sysimage/pre_build_models.jl
+++ sysimage/pre_build_models.jl
@@ -2,8 +2,13 @@ import MeshArrays
 MeshArrays.GRID_LLC90_download()
 
 import OceanStateEstimation
-OceanStateEstimation.ECCOdiags_download() 
-OceanStateEstimation.ECCOdiags_add("interp_coeffs")
+OceanStateEstimation.ECCOdiags_download()
+ 
+#OceanStateEstimation.ECCOdiags_add("interp_coeffs")
+OceanStateEstimation.Downloads.download(
+  "https://zenodo.org/record/5784905/files/interp_coeffs_halfdeg.jld2",
+  joinpath(ECCOdiags_path,"interp_coeffs_halfdeg.jld2");
+  timeout=60000.0)
 
 using PlutoSliderServer, ClimateModels
 pth=dirname(pathof(ClimateModels))
