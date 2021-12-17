f670f8ca1492c833fedb5aeeaa961714895f2fb2
gaelforget <gforget@mit.edu>
2021-12-17 00:24:58 -0500

    try without Plots.jl

--- sysimage/create_sysimage.jl
+++ sysimage/create_sysimage.jl
@@ -1,7 +1,7 @@
 using PackageCompiler
 create_sysimage([:CSV, :DataFrames, :IJulia, :Pluto, :PlutoUI, 
                 :MeshArrays, :ClimateModels, :MITgcmTools, :IndividualDisplacements, 
-                :UnicodePlots, :CairoMakie, :Plots];
+                :UnicodePlots, :CairoMakies];
                 precompile_execution_file = "sysimage/warmup.jl",
                 sysimage_path="ExampleSysimage.so",
                 cpu_target = PackageCompiler.default_app_cpu_target())
