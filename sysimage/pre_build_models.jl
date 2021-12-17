c694271cbf7a7def18d3c9c6bb811a0831e18f56
gaelforget <gforget@mit.edu>
2021-12-17 10:42:06 -0500

    fix previous commit

--- sysimage/pre_build_models.jl
+++ sysimage/pre_build_models.jl
@@ -7,7 +7,7 @@ OceanStateEstimation.ECCOdiags_download()
 #OceanStateEstimation.ECCOdiags_add("interp_coeffs")
 OceanStateEstimation.Downloads.download(
   "https://zenodo.org/record/5784905/files/interp_coeffs_halfdeg.jld2",
-  joinpath(ECCOdiags_path,"interp_coeffs_halfdeg.jld2");
+  joinpath(OceanStateEstimation.ECCOdiags_path,"interp_coeffs_halfdeg.jld2");
   timeout=60000.0)
 
 using PlutoSliderServer, ClimateModels
