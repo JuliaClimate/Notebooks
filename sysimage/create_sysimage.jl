c0aa090d749210c45e5ede667a48d92d0f138674
gaelforget <gforget@mit.edu>
2021-12-17 11:30:50 -0500

    try again with IndividualDisplacements

--- sysimage/create_sysimage.jl
+++ sysimage/create_sysimage.jl
@@ -1,6 +1,6 @@
 using PackageCompiler
 create_sysimage([:CSV, :DataFrames, :IJulia, :Pluto, :PlutoUI, 
-                :MeshArrays, :ClimateModels, :MITgcmTools,
+                :MeshArrays, :ClimateModels, :MITgcmTools, :IndividualDisplacements,
                 :UnicodePlots, :CairoMakie, :Plots];
                 precompile_execution_file = "sysimage/warmup.jl",
                 sysimage_path="ExampleSysimage.so",
