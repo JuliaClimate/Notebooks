6c4809b6e0651dbf56d62579d6b792e0ea29c09e
gaelforget <gforget@mit.edu>
2022-02-14 10:35:20 -0500

    try without IndividualDisplacements / LoopVectorization

--- sysimage/create_sysimage.jl
+++ sysimage/create_sysimage.jl
@@ -1,6 +1,6 @@
 using PackageCompiler
 create_sysimage([:CSV, :DataFrames, :IJulia, :Pluto, :PlutoUI, 
-                :MeshArrays, :ClimateModels, :MITgcmTools, :IndividualDisplacements,
+                :MeshArrays, :ClimateModels, :MITgcmTools,
                 :UnicodePlots, :CairoMakie, :Plots];
                 precompile_execution_file = "sysimage/warmup.jl",
                 sysimage_path="ExampleSysimage.so",
