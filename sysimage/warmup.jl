12fec8e066350c839854177126acb725c5eb37a9
gaelforget <gforget@mit.edu>
2022-02-25 15:06:39 -0800

    add ECCO_standard_plots.jl to sysimage warmup

--- sysimage/warmup.jl
+++ sysimage/warmup.jl
@@ -1,7 +1,5 @@
-using ClimateModels
-using MITgcmTools
-using Pluto
-using PlutoUI
+using ClimateModels, MITgcmTools, OceanStateEstimation
+using Pluto, PlutoUI, PlutoSliderServer, Downloads
 import Plots
 import CairoMakie
 
@@ -14,3 +12,6 @@ tmp=ModelConfig(model=ClimateModels.RandomWalker)
 setup(tmp)
 launch(tmp)
 
+notebook_url="https://raw.githubusercontent.com/gaelforget/OceanStateEstimation.jl/master/examples/ECCO/ECCO_standard_plots.jl"
+path_to_notebook = Downloads.download(notebook_url)
+PlutoSliderServer.export_notebook(path_to_notebook)
