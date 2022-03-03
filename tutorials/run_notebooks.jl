acc1e2aa50cba53e3266e9ae262e3fa8a4b1e5f4
gaelforget <gforget@mit.edu>
2022-03-03 05:41:22 -0500

    fix previous commit

--- tutorials/run_notebooks.jl
+++ tutorials/run_notebooks.jl
@@ -5,7 +5,7 @@ function run_one_notebook(filename::String)
     pth_in=joinpath(pwd(),"tutorials")
     pth_out=joinpath(pwd(),"page","__site")
 
-    cp(joinpath(pth_in,fil_in),joinpath(pth_out,filename))
+    cp(joinpath(pth_in,filename),joinpath(pth_out,filename))
     PlutoSliderServer.export_notebook(joinpath(pth_out,filename))
 end 
 
