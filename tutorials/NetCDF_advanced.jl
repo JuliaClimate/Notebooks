59d979c797d525db86f0e713f5da39dc51577bce
gaelforget <gforget@mit.edu>
2022-03-04 11:48:33 -0500

    fixes

--- tutorials/NetCDF_advanced.jl
+++ tutorials/NetCDF_advanced.jl
@@ -54,7 +54,7 @@ folder=joinpath(tempdir(),"NetcdfTestCase1")
 ncfile=joinpath(folder,"speeds.nc")
 
 !isdir(folder) ? mkdir(folder) : nothing
-!isfile(ncfile) ? Downloads.download(url,ncfille) : nothing
+!isfile(ncfile) ? Downloads.download(url,ncfile) : nothing
 ```
 
 """
@@ -72,7 +72,10 @@ begin
 	import NCDatasets as ncd
 	folder=joinpath(tempdir(),"NetcdfTestCase1")
 	ncfile=joinpath(folder,"speeds.nc")
-		
+
+	#open file
+	isfile(ncfile) ? ds=ncd.Dataset(ncfile) : ds=missing
+
 	max_speed(t::Int) = 
 		sqrt(maximum((ds["USFC"][:,:,t].^2 .+ ds["VSFC"][:,:,t].^2), dims = (1,2))[1])
 #		maximum(sqrt.(ds["USFC"][:,:,t].^2 .+ ds["VSFC"][:,:,t].^2), dims = (1,2))[1]
@@ -86,7 +89,6 @@ Completing the computation may take of the order of 10 seconds.
 
 # ╔═╡ b52101ba-483f-4b95-a1cf-44cc0154e8e9
 if isfile(ncfile)
-	ds=ncd.Dataset(ncfile) #open file
 	max_speed(1)
 	max_speed.(1:2400)
 else
