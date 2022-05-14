506f6b04ecf738f3886c6c6328eea5a31d55b8f7
gaelforget <gforget@mit.edu>
2022-05-14 09:30:22 -0400

    use Downloads directly

--- sysimage/download_stuff.jl
+++ sysimage/download_stuff.jl
@@ -6,7 +6,8 @@ OceanStateEstimation.ECCOdiags_download()
 OceanStateEstimation.ECCOdiags_add("release4")
 
 #OceanStateEstimation.ECCOdiags_add("interp_coeffs")
-OceanStateEstimation.Downloads.download(
+import Downloads
+Downloads.download(
   "https://zenodo.org/record/5784905/files/interp_coeffs_halfdeg.jld2",
   joinpath(OceanStateEstimation.ECCOdiags_path,"interp_coeffs_halfdeg.jld2");
   timeout=60000.0)
