7ff45c211365eaccccbf7bc699e4a64789a0ff67
gaelforget <gforget@mit.edu>
2022-05-14 09:53:33 -0400

    another try

--- sysimage/download_stuff.jl
+++ sysimage/download_stuff.jl
@@ -3,14 +3,8 @@ MeshArrays.GRID_LLC90_download()
 
 import OceanStateEstimation
 OceanStateEstimation.ECCOdiags_download()
-OceanStateEstimation.ECCOdiags_add("release4")
-
-#OceanStateEstimation.ECCOdiags_add("interp_coeffs")
-import Downloads
-Downloads.download(
-  "https://zenodo.org/record/5784905/files/interp_coeffs_halfdeg.jld2",
-  joinpath(OceanStateEstimation.ECCOdiags_path,"interp_coeffs_halfdeg.jld2");
-  timeout=60000.0)
+#OceanStateEstimation.ECCOdiags_add("release4")
+OceanStateEstimation.ECCOdiags_add("interp_coeffs")
 
 include("../tutorials/list_notebooks.jl")
 notebooks=list_notebooks()
