6724a7e85c0075eeeb392b3db19203e7ff0566ab
gaelforget <gforget@mit.edu>
2022-09-02 16:47:49 -0400

    trim down Project.coml, revisit sysimage creation

--- sysimage/warmup.jl
+++ sysimage/warmup.jl
@@ -1,18 +1,17 @@
-using ClimateModels, MITgcmTools, OceanStateEstimation
-using Pluto, PlutoUI, PlutoSliderServer, Downloads
-import Plots
-import CairoMakie
+using Pkg; 
+Pkg.test("ClimateModels")
+Pkg.test("IndividualDisplacements")
+Pkg.test("MITgcmTools")
+Pkg.test("MeshArrays")
 
-#MC=MITgcm_config(configuration="global_with_exf")
-#setup(MC)
-# build(MC,"--allow-skip")
-# launch(MC)
+using Pluto, PlutoUI, PlutoSliderServer, CairoMakie
 
+using ClimateModels
 tmp=ModelConfig(model=ClimateModels.RandomWalker)
 setup(tmp)
 launch(tmp) 
 
-notebook_url="https://raw.githubusercontent.com/gaelforget/OceanStateEstimation.jl/master/examples/ECCO/ECCO_standard_plots.jl"
-path_to_notebook = Downloads.download(notebook_url)
-#include(path_to_notebook)
+using Downloads
+url="https://raw.githubusercontent.com/JuliaClimate/MeshArrays.jl/master/examples/basics.jl"
+include(Downloads.download(url))
 
