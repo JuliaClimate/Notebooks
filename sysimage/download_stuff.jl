3924c757b995f8e572fb33e8ab2ee437948f590e
gaelforget <gforget@mit.edu>
2022-09-03 20:33:20 -0400

    fix previous commit

--- sysimage/download_stuff.jl
+++ sysimage/download_stuff.jl
@@ -16,6 +16,7 @@ path0="notebooks"
 nbs=notebooks.list()
 notebooks.download(path0,nbs)
 
+using DataFrames
 url0="https://raw.githubusercontent.com/JuliaClimate/IndividualDisplacements.jl/master/examples/worldwide/"
 nbs2=DataFrame( "folder" => ["IndividualDisplacements.jl","IndividualDisplacements.jl"], 
                 "file" => ["ECCO_FlowFields.jl","OCCA_FlowFields.jl"], 
