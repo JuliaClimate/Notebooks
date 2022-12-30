6e3cf084201fe3099c9f1aa98e10b4d06814cc4a
gaelforget <gforget@mit.edu>
2022-12-30 18:31:33 -0500

    add download_stuff and warmup

---
+++ src/warmup.jl
@@ -0,0 +1,16 @@
+
+using Dataverse
+(DataAccessApi,NativeApi)=pyDataverse.APIs()
+
+using ClimateModels
+tmp=ModelConfig(model=ClimateModels.RandomWalker)
+setup(tmp)
+launch(tmp)
+
+if false
+ using Pluto, PlutoUI, PlutoSliderServer, CairoMakie
+ fil_in=joinpath(dirname(pathof(ClimateModels)),"..", "examples","IPCC.jl")
+ PlutoSliderServer.export_notebook(fil_in)
+end
+
+
