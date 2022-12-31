0e9572fdd9e1383a1312e5719612a13eef2f6c68
gaelforget <gforget@mit.edu>
2022-12-31 01:34:28 -0500

    compile and use Dataverse

--- src/download_stuff.jl
+++ src/download_stuff.jl
@@ -1,5 +1,4 @@
 
-
 using ClimateModels
 path0="notebooks"
 nbs=notebooks.list()
@@ -11,4 +10,27 @@ nbs2=DataFrame( "folder" => ["IndividualDisplacements.jl","IndividualDisplacemen
                 "file" => ["ECCO_FlowFields.jl","OCCA_FlowFields.jl"],
                 "url" => [url0*"ECCO_FlowFields.jl",url0*"OCCA_FlowFields.jl"])
 notebooks.download(path0,nbs2)
+
+##
+
+using Pkg
+ENV["PYTHON"]=""
+Pkg.build("PyCall")
+
+using Dataverse
+(DataAccessApi,NativeApi)=pyDataverse.APIs()
+
+filenames=["exportImage_60arc.tiff","sla_podaac.nc",
+           "polygons_EEZ.geojson","polygons_MBON_seascapes.geojson"]
+pth0=joinpath(tempdir(),"azores_region_data")
+
+DOI="doi:10.7910/DVN/OYBLGK"
+df=pyDataverse.dataset_file_list(DOI)
+lst=DataverseDownloads.download_urls(df)
+
+for filename in filenames
+ file0=joinpath(pth0,filename)
+ !ispath(pth0) ? mkdir(pth0) : nothing
+ !isfile(file0) ? DataverseDownloads.download_files(lst,filename,pth0) : nothing
+end
 
