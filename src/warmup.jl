7c24891da67bb38e6beb74a71932017648d2c89b
gaelforget <gforget@mit.edu>
2023-01-03 23:27:04 -0500

    just PlutoSliderServer call

--- src/warmup.jl
+++ src/warmup.jl
@@ -4,10 +4,7 @@ tmp=ModelConfig(model=ClimateModels.RandomWalker)
 setup(tmp)
 launch(tmp)
 
-if true
- using Pluto, PlutoUI, PlutoSliderServer, CairoMakie
- fil_in=joinpath(dirname(pathof(ClimateModels)),"..", "examples","IPCC.jl")
- PlutoSliderServer.export_notebook(fil_in)
-end
-
+using PlutoSliderServer
+fil_in=joinpath("notebooks","ClimateModels.jl","IPCC.jl")
+PlutoSliderServer.export_notebook(fil_in)
 
