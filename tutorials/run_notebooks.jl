6935f470b49c6f10d1d17e50dce50df34a6b94f9
gaelforget <gforget@mit.edu>
2023-01-24 17:55:06 -0500

    add Shapefile example

--- tutorials/run_notebooks.jl
+++ tutorials/run_notebooks.jl
@@ -16,4 +16,5 @@ run_one_notebook("YAXArrays_demo.jl")
 #run_one_notebook("xarray_climarray_etc.jl")
 run_one_notebook("GeoJSON_demo.jl")
 run_one_notebook("GeoTIFF_demo.jl")
+run_one_notebook("Shapefile_demo.jl")
 
