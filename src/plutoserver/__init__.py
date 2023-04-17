29174d08077cd269a57f51356ca341658e77086c
gaelforget <gforget@mit.edu>
2023-04-17 12:01:30 -0400

    fix

--- src/plutoserver/__init__.py
+++ src/plutoserver/__init__.py
@@ -5,7 +5,7 @@ def setup_plutoserver():
     mydir = os.path.dirname(os.path.realpath(__file__))
     iconpath = os.path.join(mydir, "icons", "pluto-logo.svg")
     return {
-        "command": ["/bin/bash", "/home/jovyan/runpluto.sh", "{port}"],
+        "command": ["/bin/bash", "/usr/local/etc/gf/runpluto.sh", "{port}"],
         "timeout": 120,
         "launcher_entry": {"title": "Pluto.jl", "icon_path": iconpath},
     }
