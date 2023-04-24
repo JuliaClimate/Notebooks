2070caf7176004f81335f5b46c5fec7eeed71ff2
gaelforget <gforget@mit.edu>
2023-04-24 11:15:42 -0400

    skip notebook runs

--- src/warmup2.jl
+++ src/warmup2.jl
@@ -6,6 +6,8 @@ launch(tmp)
 
 path0="/usr/local/etc/gf/notebooks"
 
+if false
+
 using PlutoSliderServer
 #fil_in=joinpath(path0,"ClimateModels.jl","IPCC.jl")
 #PlutoSliderServer.export_notebook(fil_in)
@@ -15,4 +17,6 @@ fil_in=joinpath(path0,"IndividualDisplacements.jl","global_ocean_circulation.jl"
 PlutoSliderServer.export_notebook(fil_in)
 fil_in=joinpath(path0,"OceanStateEstimation.jl","ECCO_standard_plots.jl")
 PlutoSliderServer.export_notebook(fil_in)
+
+end
 
